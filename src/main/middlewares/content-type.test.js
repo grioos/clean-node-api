3bb2ecfb56b69f3d7850e439dea1e2303c0265e0
grios <gabriel.al.rio@gmail.com>
2020-11-25 10:50:45 -0300

    ensure requests reutnr given content-type when forced

--- src/main/middlewares/content-type.test.js
+++ src/main/middlewares/content-type.test.js
@@ -11,4 +11,15 @@ describe('Content-Type Middleware', () => {
       .post('/test_content_type')
       .expect('content-type', /json/)
   })
+
+  test('Should return xml content-type if forced', async () => {
+    app.post('/test_content_type_xml', (req, res) => {
+      res.type('xml')
+      res.send('')
+    })
+
+    await request(app)
+      .post('/test_content_type_xml')
+      .expect('content-type', /xml/)
+  })
 })
